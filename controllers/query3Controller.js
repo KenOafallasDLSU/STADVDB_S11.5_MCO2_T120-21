cc77abd5417ee8dddb5ee6b05d02fcc221432c8a
Jaspher Joya <patrick_jaspher_joya@dlsu.edu.ph>
2021-02-03 00:11:06 +0800

    Update query3Controller.js

--- controllers/query3Controller.js
+++ controllers/query3Controller.js
@@ -1,33 +1,23 @@
 const query3Model = require('../models/query3Model.js')
 
 exports.renderMain = (req, res) => {
-    
-    let promise = new Promise((resolve) => {
-        query3Model.queryDistrictNames((result) => {
-          resolve(result)
-        })
-    })
-    .then((districtList) => {
-
-        res.render('query3', {
-        pageCSS: "allQuery",
-        pageJS: "query3JS",
-        pageTitle: "Slice",
-        navbarTitle: "Slice",
-        districtList: JSON.parse(JSON.stringify(districtList)),
-
-        years: [
-            '1993',
-            '1994',
-            '1995',
-            '1996',
-            '1997',
-            '1998'
-        ],
-        result: []
-        })
+    res.render('query3', {
+    pageCSS: "allQuery",
+    pageJS: "query3JS",
+    pageTitle: "Slice",
+    navbarTitle: "Slice",
+    years: [
+        '1993',
+        '1994',
+        '1995',
+        '1996',
+        '1997',
+        '1998'
+    ],
+    result: []
     })
 }
+
 
 exports.postQuery3 = (req, res) => {
   let year = req.body.year;
